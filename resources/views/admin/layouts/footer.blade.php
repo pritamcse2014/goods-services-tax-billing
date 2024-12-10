4ff501e4005c24f8b742b02e8d89460927ab0fa9
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-12-10 23:29:48 +0600

    Admin-Login-In-Laravel-With-Layouts-Set

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024 <a target="_blank" href="https://pritamcse2014.github.io/pritamkumarkundu/">GST Billing</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block"><b>Version</b> 3.2.0</div>
+</footer>
